83535c6bc1ff154c1a04cd66d4c36bfac6272067
rafaeleneias <rafaeleneias02@gmail.com>
2023-12-13 19:39:48 -0300

    projeto realizado para trilhar os estudos

---
+++ tiposVariaveis/src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores{
+    public static void main(String[] args) {
+    // Operadores: 
+    // Operador de atribuição: =
+    // Operador aritmético: +(adição), -(subtração), *(multiplicação), /(divisão), %(resto da divisão).
+    // Operadores unários são aplicados juntamente com um outro operador aritmético. Eles realizam alguns trabalhos básicos 
+    // como incrementar, decrementar, inverter valores numéricos e booleanos.
+    // (+) Operador unário de valor positivo – números são positivos sem esse operador explicitamente;
+    // (-) Operador unário de valor negativo – nega um número ou expressão aritmética;
+    // (++) Operador unário de incremento de valor – incrementa o valor em 1 unidade;
+    // (--) Operador unário de decremento de valor – decrementa o valor em 1 unidade;
+    // (!) Operador unário lógico de negação – nega o valor de uma expressão booleana;
+    int numero = 1022;
+    System.out.println(- numero);
+    numero ++;
+    System.out.println(numero);
+    int ano = 2010;
+    System.out.println(++ ano);
+    ano = 2010;
+    System.out.println(-- ano);
+    boolean eh_verdade = true;
+    System.out.println(!eh_verdade);
+      // Operador
+    // O operador de adição(+) realizado nas strings, realiza uma concatenação entre elas.
+    }
+}
---
+++ tiposVariaveis/src/tiposVariaveis.java
@@ -0,0 +1,21 @@
+public class tiposVariaveis {
+    public static void main(String[] args) throws Exception {
+      // Variável é uma identificação de um espaço em memória utilizado pelo nosso programa. Na linguagem Java, toda variável é
+       // composta por tipo de dado + identificação + valor atribuído. Ex:
+       int idade = 20;
+       int ano = 2023;
+       String instituição = "DIO";
+       double salario = 2500.00;
+       System.out.println(idade);
+       System.out.println(ano);
+       System.out.println(instituição);
+       System.out.println(salario);
+     // Constantes são tipos de dados que não podem ser alteradas ao longo do programa. Na linguagem Java, esses valores são
+     // representadas pela palavra reservada final. Porém, por convenção, a palavr deverá ser escrita em caixa alta.Ex:
+        final int ANOATUAL = 2023; // não poderá ser mudado ao longo do programa.
+    // Char: Apenas um caractere. Ex: 'M'.
+    // Boolean: Verdadeiro ou falso.
+    // Void indica que o método não retorna nenhum valor
+
+    }
+}
